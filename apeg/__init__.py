9d68dd3c54a883becfe124120ca99346fa0ac4d7
Youngsung Kim <youngsung@ucar.edu>
2017-11-22 16:29:42 -0700

    working on real or term

---
+++ apeg/__init__.py
@@ -0,0 +1,2 @@
+from .grammar import Grammar
+from .nodes import NodeVisitor
---
+++ test/test_apeg.py
@@ -0,0 +1,21 @@
+from context import apeg
+import unittest, sys, os
+
+here = os.path.dirname(os.path.realpath(__file__))
+#grammar_test = 'fruites'
+grammar_test = 'simple'
+grammar_rules = os.path.join(here, 'grammar_%s.txt'%grammar_test)
+sample_input = os.path.join(here, 'sample_%s.txt'%grammar_test)
+
+class TestParser(unittest.TestCase):
+
+    def test_apeg(self):
+        with open(grammar_rules, 'r') as fg:
+            grammar = apeg.Grammar(fg.read())
+            with open(sample_input, 'r') as fi:
+                input_text = fi.read().strip()
+                tree = grammar.parse(input_text)
+                print(str(tree))
+
+if __name__ == '__main__':
+    unittest.main.main()
